f5295ddb37b3f2a55fc2a0ffbf6e5c8d7604bae1
Alencar Rodrigo Hentges <alencarhentges@gmail.com>
2019-04-18 02:25:39 -0300

    Removing words when not in vocabulary"

--- embedding/service/word_embedding_service.py
+++ embedding/service/word_embedding_service.py
@@ -6,6 +6,7 @@
 """
 
 import json
+import re
 
 from flask import Flask
 from flask import request
@@ -15,10 +16,9 @@ from gensim.test.utils import get_tmpfile
 
 app = Flask(__name__)
 wv = None
-VEC_FILE = "C:\\Users\\alenc\Documents\\_projetos\\ensepro\\fasttext-s50-m2-sg0.vec"
+VEC_FILE = "C:\\_a\\ensepro\\ensepro-core\\arquivos\\fasttext-s50-m2-sg0.vec"
 BINARY = False
 GLOVE = False
-
 
 @app.route('/word-embedding/similarity/', methods=['GET'])
 def similarity():
@@ -38,10 +38,18 @@ def n_similarity():
     words1 = request.args.getlist('word1')
     words2 = request.args.getlist('word2')
 
-    try:
-        score = wv.n_similarity(words1, words2)
-    except Exception as ex:
-        score = 0
+    score = 0
+    count = 0
+    while words2 and count < 10:
+        count += 1
+        try:
+            score = wv.n_similarity(words1, words2)
+            break
+        except KeyError as ex:
+            words2.remove(re.search("'(.*)'", str(ex)).group(1))
+            continue
+        except Exception as ex:
+            break
 
     return json.dumps({"score": float(str(score))})
 
