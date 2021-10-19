7ab194910bd77e2effba41bca65a9aaf52b0395a
Alencar Rodrigo Hentges <alencarhentges@gmail.com>
2021-10-19 06:50:15 -0300

    update run ensepro script

--- run_qald_ensepro.py
+++ run_qald_ensepro.py
@@ -5,6 +5,45 @@ from timeit import default_timer as timer
 from pathlib import Path
 
 
+def execEnsepro(type, file_config, jar, size=0, slm1_only_l1=False):
+    # update jar
+    configs = json.loads(open(file=file_config, mode="r", encoding="utf-8").read())
+
+    configs["cbc"]["path_answer_generator"] = jar
+    configs["cbc"]["slm1_factor"] = size
+    configs["cbc"]["slm1_factor_only_l1"] = slm1_only_l1
+
+    save_as_json(configs, file_config)
+    path = "C:/_ensepro/ensepro-similarity-service/resultados2/" + str(type) + "/"
+    if size > 0:
+        path = path + str(slm1_only_l1) + "/" + str(size) + "/"
+    Path(path).mkdir(parents=True, exist_ok=True)
+    i = 0
+    for frase in frases:
+        filename = "{:0>3d}-v2-s{size}-".format(i, size=size) + frase.replace("?", "")
+        base_command = "python C:\_ensepro\ensepro-core\main\ensepro_main.py "
+        params_command = "-save-json -filename \"{filename}\" -frase \"{frase}\" -resposta"
+
+        final_command = base_command + params_command.format(frase=frase, filename=path + filename)
+
+        start = time.time_ns()
+        os.system(final_command)
+        end = time.time_ns()
+
+        try:
+            response_file = json.loads(open(file=path + filename + ".json", mode="r", encoding="utf-8").read())
+
+            if not response_file[0]["resposta"]:
+                response_file[0]["resposta"] = {}
+
+            #response_file[0]["resposta"]["total_time"] = (end - start) * 1000  # converts to ms
+            response_file[0]["resposta"]["total_time"] = (end - start)  # in nanoseconds
+            save_as_json(response_file, path + filename + ".json")
+        except Exception as e:
+            print(e)
+        i += 1
+
+
 def save_as_json(value, filename, indent=2, sort_keys=False):
     import json
     print(
@@ -27,47 +66,16 @@ def carregar_frases(arquivo):
 
     return frases
 
-base_jar = "C:\_ensepro\ensepro-answer-generator\ensepro-answer-generator-size-{size}.jar"
+
+base_jar = "C:\_ensepro\ensepro-answer-generator\jars\ensepro-answer-generator-size-base.jar"
+slm1_jar = "C:\_ensepro\ensepro-answer-generator\jars\ensepro-answer-generator-size-slm1.jar"
 configs_file = "C:\\_ensepro\\ensepro-core\\ensepro\\configuracoes\\configs.json"
 frases = carregar_frases("qald7.txt")
-jars_type = ["base", "slm1"]
-slm1_only_l1_options = [True, False]
-sizes = [10, 50, 75, 100, 150, 200, 300, 400, 500, 750, 1000, 2000, 3000, 4000]
+slm1_only_l1_options = [False]
+sizes = [10, 50, 75, 100, 150, 200, 300, 400, 500, 750, 1000, 2000]
+#sizes = [10, 50]
 
-for jar in jars_type:
-    for slm1_only_l1 in slm1_only_l1_options:
-        for size in sizes:
-            # update jar
-            configs = json.loads(open(file=configs_file, mode="r", encoding="utf-8").read())
-
-            configs["cbc"]["path_answer_generator"] = base_jar.format(size=jar)
-            configs["cbc"]["slm1_factor"] = size
-            configs["cbc"]["slm1_factor_only_l1"] = slm1_only_l1
-
-            save_as_json(configs, configs_file)
-            path = "C:/_ensepro/ensepro-similarity-service/resultados2/"+str(jar)+ "/"+str(slm1_only_l1)+"/"+str(size)
-            Path(path).mkdir(parents=True, exist_ok=True)
-            
-            for frase in frases:
-                filename = "{:0>3d}-v2-s{size}-".format(i, size=size) + frase.replace("?", "")
-                base_command = "python C:\_ensepro\ensepro-core\main\ensepro_main.py "
-                params_command = "-save-json -filename \"{filename}\" -frase \"{frase}\" -resposta"
-
-                final_command = base_command + params_command.format(frase=frase, filename=path + filename)
-
-                start = time.time()
-                os.system(final_command)
-                end = time.time()
-
-                try:
-                    response_file = json.loads(open(file=path + filename + ".json", mode="r", encoding="utf-8").read())
-
-                    if not response_file[0]["resposta"]:
-                        response_file[0]["resposta"] = {}
-
-                    response_file[0]["resposta"]["total_time"] = (end - start) * 1000  # converts to ms
-                    save_as_json(response_file, path + filename + ".json")
-                except Exception as e:
-                    print(e)
-
-                i += 1
+for size in sizes:
+    # for slm1_only_l1 in slm1_only_l1_options:
+    execEnsepro("slm1", configs_file, slm1_jar, size, False)
+execEnsepro("base", configs_file, base_jar)
